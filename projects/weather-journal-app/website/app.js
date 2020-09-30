e9beb820347f69af85af0943fa892b5247d3de69
martin-webs <69458165+martin-webs@users.noreply.github.com>
2020-09-29 19:37:32 -0700

    Update app.js
    Added getWeather function, and updateUi; still get the message "document not defined"; UI does not update.

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,18 +1,12 @@
 /* Global Variables */
-// const apiKey = '?';
-// const zipCode = '10001';
-// const owmUrl = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}`
-//api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
 //  Create a new date instance dynamically with JS
-// let d = new Date();
-// let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let d = new Date();
+let ndate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKEY = ',us&appid=f39412133af70263bacfa937b826adca&units=imperial';
 
-
-/* Function to POST data */
-//Post Data
-const fetch = require('node-fetch');
-const postData = async (url = "", data = {}) => {
+const postData = async (url = "", data = {} ) => {
   const response = await fetch(url, {
     method: "POST",
     credentials: "same-origin",
@@ -22,19 +16,48 @@ const postData = async (url = "", data = {}) => {
     body: JSON.stringify(data),
   });
   try {
-    const newData = await response.json();
-    return newData;
+    const newData1 = await response.json();
+    return newData1;
   } catch (error) {
     console.log("error ", error);
   }
-};
+}
 
-postData('https://localhost:8000/dataRoute', {name: 'Joe'})
-.then(result => {
-  console.log(result);
-}).catch(error => {
-  console.log(error);
+const getWeather = async (url ="", zip, key) => {
+    const requestWeather = await fetch (url+zip+key);
+    try {
+      const data = await requestWeather.json();
+      return data;
+    } catch (error) {
+      console.log("error", error);
+    }
+}
+
+const submitButton = document.getElementById('generate');
+
+submitButton.addEventListener('click', () => {
+    const zipCode = document.getElementById('zip').value;
+    const feeling = document.getElementById('feelings').value;
+    getWeather(baseURL, zipCode, apiKEY)
+        .then((data) => {
+            console.log(data);
+            postData('dataRoute', { date: ndate, temp: data.main.temp, feeling: feeling }); 
+          }).then(function () {
+            updateUi();
+        });
 });
 
-  // TODO-Call Function  
+const updateUi = async () => {
+  console.log('updateUi up and running');
+  const request = await fetch('dataRoute');
+  try {
+    const allData = request.json();
+    console.log('try started');
+    document.getElementById('date-span').innerHTML = allData.date;
+    document.getElementById('temp-span').innerHTML = allData.temperature;
+    document.getElementById('feel-span').innerHTML = allData.feeling;
+  } catch (error) {
+      console.log("error",error);
+  }
+}
 
